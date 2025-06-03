b74ade2f4963a5ecb90027383fa8cb6f8d16354b
Rythm Chawla <120208857+RythmChawla@users.noreply.github.com>
2025-06-03 11:12:10 +0530

    Update chargingStations.js

--- backend/Routes/chargingStations.js
+++ backend/Routes/chargingStations.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ChargingStation = require("../Models/ChargingStation");
+const ChargingStation = require("../models/ChargingStation");
 const ensureAuthenticated = require("../Middlewares/Auth");
 
 // CREATE - Add new charging station
